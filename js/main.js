a19ef54b26368b217168217dfe14e29bc656f7c8
Romina <roominaangel@gmail.com>
2025-01-08 20:50:05 -0300

    cambio de foto

--- js/main.js
+++ js/main.js
@@ -9,3 +9,36 @@ function toggleDropdown() {
     const dropdownContent = document.querySelector(".dropdown .dropdown-content");
     dropdownContent.classList.toggle("show-dropdown");
 }
+
+
+//carrusel//
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentIndex = 0;
+
+// Función para mover el carrusel
+const moveToSlide = (track, currentIndex) => {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+};
+
+// Evento de clic en "siguiente"
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    moveToSlide(track, currentIndex);
+});
+
+// Evento de clic en "anterior"
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    moveToSlide(track, currentIndex);
+});
+
+// Ajuste del carrusel al redimensionar la ventana
+window.addEventListener('resize', () => {
+    moveToSlide(track, currentIndex);
+});
